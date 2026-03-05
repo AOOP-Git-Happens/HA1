f4ccd2c2dc8038cfc8a5360d287a2ed86fb86728
seekerowls <v.wegener@freenet.de>
2026-03-05 13:36:54 +0100

    txt file update

--- AreaControlGame/Models/SaveFile.cs
+++ AreaControlGame/Models/SaveFile.cs
@@ -1,8 +1,16 @@
 // Idea: this file is for loading and saving the current situation. Bassicly it needs to convert the boards situation into the txt or the other way around
 
 using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.IO;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 using System.Text;
+using System.Threading.Channels;
+using Microsoft.VisualBasic;
 
 namespace AreaControlGame.Models;
 
@@ -13,11 +21,28 @@ cell0 cell1 cell3 cell4 cellN
 1 - player1,
 2 - player2
 */
-public static class SaveFile
+
+
+//public static class SaveFile
+public class SaveFile
 {
+    public int Row { get; } 
+    public int Column { get; }
+    public GameState CurrentGame { get; }
     public static void SaveToFile(string path, GameState game)
     {
-        throw new NotImplementedException();
+        int [] flatArray = new int [Row * Coloumn];
+        for (int i = 0; i < Row; i++)
+        {
+            for (int y = 0; y < Column; y++)
+            {
+                int cell = CurrentGame.GetCell(i, y);
+                flatArray[index++] = cell;
+            }
+        }
+        string output = string.Join(" ", flatArray);
+        File.WriteAllText("settings.txt", output);
+        //throw new NotImplementedException();
     }
 
     public static GameState LoadFromFile(string path)
